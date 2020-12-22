67910e40eae407548417ebca88e312623a6b789b
alessandro <alessandro.massarella@gmail.com>
2020-12-22 18:47:25 +0100

    Snack1 + Snack2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!-- Esercizio 2 - PHP Loops
 repo: php-snacks-blocco-1
+
 PHP Snack 1:
 Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
@@ -50,5 +51,67 @@ for ($i = 0; $i < 4; $i++) {
     
     . "<br />";
 }
+
+
+
+
+
+
+// PHP Snack 2:
+
+
+// Passare come parametri GET name, mail e age everificare (cercando i metodi che non conosciamo nella documentazione) che:
+// 1. name sia più lungo di 3 caratteri,
+// 2. che mail contenga un punto e una chiocciola
+// 3. e che age sia un numero.
+// Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+
+var_dump($name, $mail, $age);
+
+// rilevo la lunghezza della stringa name
+$str_length = strlen($name);
+var_dump($str_length);
+
+// controllo che la mail contenga il punto e la chiocciola
+$mail_check_a = (strpos($mail, '@') !== false);
+var_dump($mail_check_a);
+               
+
+$mail_check_b = (strpos($mail, '.') !== false);
+var_dump($mail_check_b);
+              
+
+
+// controllo che age sia un numero
+$age_is_int= is_numeric($age);
+var_dump($age_is_int);
+
+
+if ($str_length > 2 and 
+$age_is_int == true and 
+$mail_check_a == true and 
+$mail_check_b == true) {
+    echo "ACCESSO RIUSCITO";
+} 
+
+else {
+    echo "ACCESSO NEGATO";
+}
+
+
+
+
+
+
+
+
+
+
+
 ?>
 
